51ef7562c9fb008323f4a7ff468b8c36e24c50cb
Hajime Suzuki <hajime.doublebass@gmail.com>
2018-09-13 14:41:09 +0200

    loginform

--- client/src/components/login/LoginForm.js
+++ client/src/components/login/LoginForm.js
@@ -1,12 +1,40 @@
 import React from 'react'
 import { Field, reduxForm } from 'redux-form'
+import TextField from '@material-ui/core/TextField'
+import Grid from '@material-ui/core/Grid'
+import Button from '@material-ui/core/Button'
+import styled from 'styled-components'
+
+const renderTextField = ({ input, meta: { touched, error }, ...custom }) => (
+  <TextField {...input} {...custom} />
+)
+
+const StyledGrid = styled(Grid)`
+  .item {
+    margin-bottom: 2em;
+  }
+`
 
 const LoginForm = props => {
   return (
     <form onSubmit={props.handleSubmit}>
-      <Field type="text" component="input" name="email" />
-      <Field type="text" component="input" name="password" />
-      <button type="submit">submit</button>
+      <StyledGrid container direction="column">
+        <Grid item className="item">
+          <Field component={renderTextField} name="email" label="email" />
+        </Grid>
+        <Grid item className="item">
+          <Field
+            type="password"
+            component={renderTextField}
+            name="password"
+            label="password"
+          />
+        </Grid>
+      </StyledGrid>
+
+      <Button type="submit" variant="contained" color="primary">
+        submit
+      </Button>
     </form>
   )
 }
