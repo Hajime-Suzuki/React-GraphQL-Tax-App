854a7f068bc12395179f64385b01f252aea6cd93
Hajime Suzuki <hajime.doublebass@gmail.com>
2018-11-24 12:33:03 +0100

    authentication

--- server/src/GraphQL/user/resolvers.ts
+++ server/src/GraphQL/user/resolvers.ts
@@ -1,11 +1,18 @@
 import { GetUserQueryArgs } from '../@types/types'
 import { IResolverObject } from 'graphql-tools'
-import { User } from '../../Models/User'
+import { User, IUser } from '../../Models/User'
 import { Types } from 'mongoose'
+import { Context } from 'koa'
+import { AuthenticationError } from 'apollo-server-koa'
+
+interface ICtx {
+  userId: string
+}
 
 export const userResolvers: IResolverObject = {
   Query: {
-    async getUser(_, { id }: GetUserQueryArgs) {
+    async getUser(_, { id }: GetUserQueryArgs, { userId }: ICtx) {
+      if (!userId) throw new AuthenticationError('You are not authorized')
       const user = await User.findById({ _id: id }).populate({
         path: 'projects',
         options: { sort: { date: -1 } },
