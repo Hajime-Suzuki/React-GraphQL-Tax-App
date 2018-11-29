f27ebde5fde61510f7c0f81ee5ea48a2072508a6
Hajime Suzuki <hajime.doublebass@gmail.com>
2018-11-30 00:26:16 +0100

    change lock file

--- server/src/server.ts
+++ server/src/server.ts
@@ -21,59 +21,12 @@ const typeDefs = mergeTypes(
   }
 )
 
-// const myCustomScalarType = new GraphQLScalarType({
-//   name: 'MyCustomScalar',
-//   description: 'Description of my custom scalar type',
-//   serialize() {
-//     // Implement your own behavior here by setting the 'result' variable
-//     return 'asht'
-//   },
-//   parseValue() {
-//     // Implement your own behavior here by setting the 'result' variable
-//     return 'qdrw'
-//   },
-//   parseLiteral(ast) {
-//     return 'zvmc'
-//   }
-// })
-
-// const schemaString = `
-
-// scalar MyCustomScalar
-
-// type Foo {
-//   aField: MyCustomScalar
-// }
-
-// type Query {
-//   foo: Foo
-// }
-
-// `
-// const resolverFunctions = {
-//   MyCustomScalar: myCustomScalarType
-// }
-// console.log(typeDefs + 'scalar Test')
-// console.log({
-//   ...userResolvers,
-//   ...projectResolvers,
-//   test: customScalar
-// })
-// console.log(
-//   makeExecutableSchema({
-//     typeDefs,
-//     resolvers: [userResolvers, projectResolvers, customScalar]
-//   })
-// )
 const server = new ApolloServer({
   schema: makeExecutableSchema({
     typeDefs,
     resolvers: [userResolvers, projectResolvers]
   }),
-  // schema: makeExecutableSchema({
-  //   typeDefs: schemaString,
-  //   resolvers: resolverFunctions
-  // }),
+
   context: async ({ ctx: { headers } }: { ctx: Context }) => {
     if (headers.jwt) {
       const user = await User.findByToken(headers.jwt)
