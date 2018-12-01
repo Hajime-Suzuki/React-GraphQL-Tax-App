e9ad6cced9177940df6d44c713e1fceeaed380c7
Hajime Suzuki <hajime.doublebass@gmail.com>
2018-12-01 20:07:29 +0100

    fix http link

--- client/src/graphql/client.ts
+++ client/src/graphql/client.ts
@@ -2,13 +2,13 @@ import { InMemoryCache } from 'apollo-cache-inmemory'
 import { ApolloClient } from 'apollo-client'
 import { ApolloLink } from 'apollo-link'
 import { setContext } from 'apollo-link-context'
-import { createHttpLink } from 'apollo-link-http'
+import { HttpLink } from 'apollo-link-http'
 import { withClientState } from 'apollo-link-state'
 import { getJwt, USER_ID } from 'src/libs/jwt'
 import { resolvers } from './resolvers'
 import { typeDefs } from './typeDefs'
 
-const httpLink = createHttpLink({
+const httpLink = new HttpLink({
   uri: 'http://localhost:4000/graphql'
 })
 
