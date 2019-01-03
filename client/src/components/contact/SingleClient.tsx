46d2ace7b1a806c870db94b58b650538a9c0a310
Hajime Suzuki <hajime.doublebass@gmail.com>
2019-01-03 23:52:34 +0100

    Single client componet

---
+++ client/src/components/contact/SingleClient.tsx
@@ -0,0 +1,65 @@
+import * as React from 'react'
+import { SingleClient as SC } from 'src/graphql/components/clients'
+import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid'
+import styled from 'styled-components'
+import Avatar from '@material-ui/core/Avatar'
+import { theme } from 'src/styles/theme'
+import Typography from '@material-ui/core/Typography'
+import Divider from '@material-ui/core/Divider'
+
+interface OwnProps {
+  client: SC.GetSingleClient
+}
+
+const Container: any = styled(Grid)`
+  .card {
+    padding: 2em;
+    text-align: center;
+  }
+  .avatar {
+    background-color: ${theme.palette.secondary.main};
+    width: 70;
+    height: 70;
+    margin: auto;
+    margin-bottom: 1em;
+  }
+  .divider {
+    margin: 1em 0;
+  }
+`
+const SingleClient: React.SFC<OwnProps> = props => {
+  const {
+    firstName,
+    lastName,
+    email,
+    phone,
+    streetAddress,
+    postalCode,
+    city
+  } = props.client
+
+  return (
+    <Container container justify="center">
+      <Grid item xs={11} md={5}>
+        <Paper className="card">
+          <Avatar className="avatar">
+            {firstName && firstName.slice(0, 1).toUpperCase()}
+            {lastName && lastName.slice(0, 1).toUpperCase()}
+          </Avatar>
+          <Typography variant="h5">
+            {firstName} {lastName}
+          </Typography>
+          <Divider className="divider" />
+          <Typography variant="subtitle1">{email}</Typography>
+          <Typography variant="subtitle1">{phone}</Typography>
+          <Typography variant="subtitle1">
+            {streetAddress}, {postalCode} {city}
+          </Typography>
+        </Paper>
+      </Grid>
+    </Container>
+  )
+}
+
+export default SingleClient
--- client/src/components/contact/SingleClientContainer.tsx
+++ client/src/components/contact/SingleClientContainer.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react'
 import { RouteComponentProps } from 'react-router'
-import { SingleClient } from 'src/graphql/components/clients'
+import { SingleClient as SingleC } from 'src/graphql/components/clients'
 import { LoadingIcon } from '../UI/LoadingIcon'
+import SingleClient from './SingleClient'
 
-type Props = SingleClient.Props<RouteComponentProps<{ clientId: string }>>
+type Props = SingleC.Props<RouteComponentProps<{ clientId: string }>>
+
 class SingleClientContainer extends React.Component<Props> {
   render() {
     const { data } = this.props
@@ -12,10 +14,10 @@ class SingleClientContainer extends React.Component<Props> {
     if (error) return error.message
     if (loading) return <LoadingIcon />
     if (!client) return 'No client found'
-    return client.firstName
+    return <SingleClient client={client} />
   }
 }
 
-export default SingleClient.HOC<Props>({
+export default SingleC.HOC<Props>({
   options: ({ match }) => ({ variables: { id: match.params.clientId } })
 })(SingleClientContainer)
