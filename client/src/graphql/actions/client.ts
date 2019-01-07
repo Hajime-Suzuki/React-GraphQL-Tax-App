ecd0e623e0ffe0deb674fc7b28c24a8332c3d115
Hajime Suzuki <hajime.doublebass@gmail.com>
2019-01-08 00:29:02 +0100

    add ClientAction

--- client/src/graphql/actions/client.ts
+++ client/src/graphql/actions/client.ts
@@ -1,5 +1,28 @@
-const getClientAfterUpdateSingleProject = () => {}
+import { client } from '../client'
+import { GetSingleProject } from '../components/projects'
+import { SingleClient } from '../components/clients'
+const attachClientToProject = (clientId: string, projectId: string) => {
+  const data = client.readQuery<
+    GetSingleProject.Query,
+    GetSingleProject.Variables
+  >({
+    query: GetSingleProject.Document,
+    variables: { id: projectId }
+  })
+  const clientData = client.readQuery<
+    SingleClient.Query,
+    SingleClient.Variables
+  >({ query: SingleClient.Document, variables: { id: clientId } })
+  console.log(clientId)
+
+  client.writeQuery({
+    query: GetSingleProject.Document,
+    data: { ...data, client: clientData }
+  })
+
+  console.log(test)
+}
 
 export const ClientAction = {
-  getClientAfterUpdateSingleProject
+  attachClientToProject
 }
