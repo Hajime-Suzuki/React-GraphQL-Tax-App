f74afb460f6eb6708c78b818ec5bde8f84bc7222
Hajime-Suzuki <hajime.doublebass@gmail.com>
2019-01-31 23:10:36 +0100

    update private routes

--- client/src/routes/PrivateRoutes.tsx
+++ client/src/routes/PrivateRoutes.tsx
@@ -1,28 +1,50 @@
 import * as React from 'react'
-import { Redirect, Route, RouteProps } from 'react-router-dom'
-import { RoutesNames } from './constants'
-import { GetToken } from 'src/graphql/components/client/login'
-import { PrivateRoutesChildProps } from './types'
+import {
+  Route,
+  RouteComponentProps,
+  RouteProps,
+  withRouter
+} from 'react-router-dom'
+import { GetUser } from 'src/graphql/components/login'
+import { LoginActions } from '../graphql/actions/login'
 
-class PrivateRoutes extends React.Component<
-  GetToken.Props<RouteProps> & PrivateRoutesChildProps
-> {
+type Props = GetUser.Props<RouteComponentProps & RouteProps>
+class PrivateRoutes extends React.Component<Props> {
+  logoutAndRedirect = () => {
+    LoginActions.logout().then(() => {
+      this.props.history.replace('/')
+    })
+    return null
+  }
+
   render() {
     const { data, component, ...rest } = this.props
-    const userId = data!.userId
+    if (!data || data.loading) return null
 
-    if (!userId) {
-      return <Redirect to={RoutesNames.login} />
+    const isNetworkErorr = data.error && data.error.networkError
+    const isAuthError =
+      isNetworkErorr &&
+      (isNetworkErorr as any).result.errors
+        .map((e: any) => e.extensions.code)
+        .includes('UNAUTHENTICATED')
+
+    if (isAuthError) return this.logoutAndRedirect()
+
+    if (isNetworkErorr) {
+      return 'Network Error'
     }
+
+    if (!data.getUser || (data.getUser && !data.getUser.id))
+      return this.logoutAndRedirect()
 
     const Component = component as any
     return (
       <Route
         {...rest}
-        render={props => <Component {...props} userId={userId} />}
+        render={props => <Component {...props} userId={data.getUser!.id} />}
       />
     )
   }
 }
 
-export default GetToken.HOC<RouteProps>({})(PrivateRoutes)
+export default withRouter(GetUser.HOC<Props>({})(PrivateRoutes))
