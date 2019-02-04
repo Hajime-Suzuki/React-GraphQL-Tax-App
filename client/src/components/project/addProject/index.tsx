49e89714665f02f434803019dc723d706d359f56
Hajime-Suzuki <hajime.doublebass@gmail.com>
2019-02-04 21:57:17 +0100

    add react-adopt to add project

--- client/src/components/project/addProject/index.tsx
+++ client/src/components/project/addProject/index.tsx
@@ -1,15 +1,14 @@
 import { Formik, FormikActions, FormikProps } from 'formik'
 import * as React from 'react'
+import { adopt } from 'react-adopt'
+import { MutationFn, MutationResult, QueryResult } from 'react-apollo'
+import { ClientAction } from 'src/graphql/actions/client'
 import { ProjectActions } from 'src/graphql/actions/projects'
 import { GetClientsList } from 'src/graphql/components/clients'
 import { AddProject, ProjectInput } from 'src/graphql/components/projects'
 import { projectValidationSchemas } from '../helper/validationSchemas'
 import AddProjectForm from './AddProjectForm'
 import { getSelectedClient } from './selector'
-import { ClientAction } from 'src/graphql/actions/client'
-import { adopt } from 'react-adopt'
-import { AddProjectRPC, RenderProps } from './containers'
-import Typography from '@material-ui/core/Typography'
 
 export type AddProjectChildProps = {
   clients?: GetClientsList.GetClientsByUser[] | null
@@ -19,120 +18,117 @@ export type AddProjectChildProps = {
   selectedClient?: GetClientsList.GetClientsByUser
 } & FormikProps<ProjectInput>
 
-// const Composed = adopt<{ container: RenderProps }, {}>({
-//   container: <AddProjectRPC />
-// })
-
-class AddProjectContainer extends React.Component<GetClientsList.Props<{}>> {
+export default class AddProjectContainer extends React.Component {
   render = () => {
-    const { data } = this.props
-    if (!data) return null
-
-    const { getClientsByUser: clients } = data
-
     return (
       <AddProjectRPC>
-        {({ addProjectForm }: RenderProps) => {
-          console.log(addProjectForm.values)
-          return <div>test</div>
-          // return (
-          //   <Formik
-          //     onSubmit={async (
-          //       values: ProjectInput,
-          //       { resetForm }: FormikActions<ProjectInput>
-          //     ) => {
-          //       await addProject!({
-          //         variables: {
-          //           data: values
-          //         }
-          //       })
-          //       resetForm()
-          //     }}
-          //     validateOnChange={false}
-          //     validateOnBlur={false}
-          //     initialValues={addProjectInitialValues}
-          //     validationSchema={
-          //       projectValidationSchemas.addProjectValidationSchema
-          //     }
-          //     render={(formProps: FormikProps<ProjectInput>) => (
-          //       <AddProjectForm
-          //         clients={clients}
-          //         mutationError={mutationError && 'something went wrong'}
-          //         loading={loading}
-          //         successMessage={
-          //           data && data.addProject && data.addProject.message
-          //         }
-          //         selectedClient={getSelectedClient({
-          //           clientsList: clients,
-          //           clientFormInput: formProps.values.client
-          //         })}
-          //         {...formProps}
-          //       />
-          //     )}
-          //   />
-          // )
+        {({
+          getClients: { data: cData },
+          addProject: { result },
+          addProjectForm: formProps
+        }: RenderProps) => {
+          const clients = cData && cData.getClientsByUser
+          const {
+            error: mutationError,
+            loading: mutationLoading,
+            data
+          } = result
+
+          return (
+            <AddProjectForm
+              clients={clients}
+              mutationError={mutationError && 'something went wrong'}
+              loading={mutationLoading}
+              successMessage={
+                data && data.addProject && data.addProject.message
+              }
+              selectedClient={getSelectedClient({
+                clientsList: clients,
+                clientFormInput: formProps.values.client
+              })}
+              {...formProps}
+            />
+          )
         }}
       </AddProjectRPC>
     )
-    //   {(addProject, { data, error: mutationError, loading }) => {
-    //     return (
-    //       <Formik
-    //         onSubmit={async (
-    //           values: ProjectInput,
-    //           { resetForm }: FormikActions<ProjectInput>
-    //         ) => {
-    //           await addProject!({
-    //             variables: {
-    //               data: values
-    //             }
-    //           })
-    //           resetForm()
-    //         }}
-    //         validateOnChange={false}
-    //         validateOnBlur={false}
-    //         initialValues={addProjectInitialValues}
-    //         validationSchema={
-    //           projectValidationSchemas.addProjectValidationSchema
-    //         }
-    //         render={(formProps: FormikProps<ProjectInput>) => (
-    //           <AddProjectForm
-    //             clients={clients}
-    //             mutationError={mutationError && 'something went wrong'}
-    //             loading={loading}
-    //             successMessage={
-    //               data && data.addProject && data.addProject.message
-    //             }
-    //             selectedClient={getSelectedClient({
-    //               clientsList: clients,
-    //               clientFormInput: formProps.values.client
-    //             })}
-    //             {...formProps}
-    //           />
-    //         )}
-    //       />
-    //     )
-    //   }}
-    // }
   }
 }
 
-// export const addProjectInitialValues = {
-//   invoiceNumber: '',
-//   invoiceDate: '',
-//   name: '',
-//   projectDate: '',
-//   incomes: [],
-//   expenses: [],
-//   client: {
-//     id: '',
-//     firstName: '',
-//     lastName: '',
-//     email: '',
-//     phone: '',
-//     postalCode: '',
-//     streetAddress: '',
-//     city: ''
-//   }
-// }
+const getClients = ({ render }: any) => (
+  <GetClientsList.Component>{render}</GetClientsList.Component>
+)
 
-export default GetClientsList.HOC({})(AddProjectContainer)
+const addProject = ({ render }: any) => {
+  return (
+    <AddProject.Component
+      onCompleted={data => {
+        if (!data.addProject) return null
+        ProjectActions.addNewProjectToList(data)
+        ProjectActions.sortProjectsByProjectDate('-1')
+        const client = data.addProject.client
+        if (client) ClientAction.addClient(client)
+      }}
+    >
+      {(mutation, result) => render({ mutation, result })}
+    </AddProject.Component>
+  )
+}
+
+const addProjectForm = ({
+  addProject: { mutation },
+  render
+}: RenderProps & { render: any }) => (
+  <Formik
+    onSubmit={async (
+      values: ProjectInput,
+      { resetForm }: FormikActions<ProjectInput>
+    ) => {
+      await mutation!({
+        variables: {
+          data: values
+        }
+      })
+      resetForm()
+    }}
+    validateOnChange={false}
+    validateOnBlur={false}
+    initialValues={addProjectInitialValues}
+    validationSchema={projectValidationSchemas.addProjectValidationSchema}
+    render={render}
+  />
+)
+
+interface RenderProps {
+  getClients: QueryResult<GetClientsList.Query, GetClientsList.Variables>
+  addProject: {
+    mutation: MutationFn<AddProject.Mutation, AddProject.Variables>
+    result: MutationResult<AddProject.Mutation>
+  }
+  addProjectForm: FormikProps<ProjectInput>
+}
+
+const AddProjectRPC = adopt<RenderProps, any>({
+  getClients,
+  addProject,
+  addProjectForm
+})
+
+export const addProjectInitialValues = {
+  invoiceNumber: '',
+  invoiceDate: '',
+  name: '',
+  projectDate: '',
+  incomes: [],
+  expenses: [],
+  client: {
+    id: '',
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+    postalCode: '',
+    streetAddress: '',
+    city: ''
+  }
+}
